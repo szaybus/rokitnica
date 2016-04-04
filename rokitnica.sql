df8b7d96acfe6be1f7c4e7039e58a4b4c57a7b87
szaybus <szaybus@gmail.com>
2016-04-04 11:39:22 +0200

    baza
    wrzucam baze

---
+++ rokitnica.sql
@@ -0,0 +1,97 @@
+-- phpMyAdmin SQL Dump
+-- version 4.4.14
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Czas generowania: 04 Kwi 2016, 11:38
+-- Wersja serwera: 5.5.42
+-- Wersja PHP: 5.5.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `rokitnica`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `building`
+--
+
+CREATE TABLE IF NOT EXISTS `building` (
+  `id_building` int(11) NOT NULL,
+  `type` int(11) NOT NULL,
+  `level` int(11) NOT NULL,
+  `name` varchar(40) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Zrzut danych tabeli `building`
+--
+
+INSERT INTO `building` (`id_building`, `type`, `level`, `name`) VALUES
+(1, 5, 5, 'Spichlerz'),
+(2, 7, 1, 'Zagroda');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `village`
+--
+
+CREATE TABLE IF NOT EXISTS `village` (
+  `id_village` int(11) NOT NULL,
+  `food` int(11) NOT NULL,
+  `wood` int(11) NOT NULL,
+  `iron` int(11) NOT NULL,
+  `clay` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Zrzut danych tabeli `village`
+--
+
+INSERT INTO `village` (`id_village`, `food`, `wood`, `iron`, `clay`) VALUES
+(1, 200, 300, 400, 500);
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indexes for table `building`
+--
+ALTER TABLE `building`
+  ADD PRIMARY KEY (`id_building`);
+
+--
+-- Indexes for table `village`
+--
+ALTER TABLE `village`
+  ADD PRIMARY KEY (`id_village`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `building`
+--
+ALTER TABLE `building`
+  MODIFY `id_building` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT dla tabeli `village`
+--
+ALTER TABLE `village`
+  MODIFY `id_village` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
